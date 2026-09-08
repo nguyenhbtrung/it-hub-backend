0758cca511d7191c72ea8798b9e7a2b012710a6c
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-09-08 17:04:20 +0700

    refactor(cache): remove UserCache and update index exports

--- src/infra/cache/index.ts
+++ src/infra/cache/index.ts
@@ -2,5 +2,4 @@ export * from './course';
 
 export { CategoryCache } from './category';
 export { StepCache } from './step';
-export { UserCache } from './user.cache';
 export { RefreshTokenCache } from './refreshToken.cache';
